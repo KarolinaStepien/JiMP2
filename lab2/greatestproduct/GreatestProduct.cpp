75620dd21ecce867236c8fd79b24900aab7007a4
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-03-15 21:14:55 +0100

    lab1test

--- lab2/greatestproduct/GreatestProduct.cpp
+++ lab2/greatestproduct/GreatestProduct.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "GreatestProduct.h"
+
+int GreatestProduct(const vector <int> &numbers, int k)
+{
+    int max1=numbers[0], max2=numbers[0], tmp;
+    for(int v : numbers)
+    {
+        if(v>max1){
+            max1 = v;
+        }
+        if(max2<max1){
+            tmp = max2;
+            max2 = max1;
+            max1 = tmp;
+        }
+    }
+
+    return max1*max2;
+}
