c076a74d48a423569aa8daecdfb8af2c6206e1a7
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-03-21 08:29:03 +0100

    tinyurl1()

---
+++ lab3/tinyurl/TinyUrl.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Admin on 2017-03-21.
+//
+
+#include "TinyUrl.h"
+
+namespace tinyurl{
+
+    void NextHash(std::array<char, 6> *state)
+    {
+        int it[6];
+        for(int i=0;i<6;i++)
+        {
+            it[i] = (*state)[i];
+        }
+        it[5]++;
+        for(int i=5;i>=0;i--)
+        {
+            if(it[i]==91)
+            {
+                it[i]=97;
+                break;
+            }
+            if(it[i]==58)
+            {
+                it[i]=65;
+                break;
+            }
+
+            if(it[i]==123 && i!=0)
+            {
+                it[i]=48;
+                it[i-1]++;
+            }
+        }
+        for(int i=0;i<6;i++)
+        {
+            (*state)[i] = char(it[i]);
+        }
+    }
+
+
+}
---
+++ lab3/tinyurl/TinyUrl.h
@@ -0,0 +1,26 @@
+//
+// Created by Admin on 2017-03-21.
+//
+
+#ifndef JIMP_EXERCISES_TINYURL_H
+#define JIMP_EXERCISES_TINYURL_H
+
+
+#include <utility>
+#include <string>
+#include <array>
+#include <memory>
+#include <iostream>
+
+
+
+namespace tinyurl {
+    using namespace std;
+
+//unique_ptr<TinyUrlCodec> Init();
+    void NextHash(array<char, 6> *state);
+//string Encode(const string &url, unique_ptr<TinyUrlCodec> *codec);
+//string Decode(const unique_ptr<TinyUrlCodec> &codec, const string &hash);
+}
+
+#endif //JIMP_EXERCISES_TINYURL_H
