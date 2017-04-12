f02be7e609de4ebbf0b280469bcbb79c8911e4cf
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-04-13 00:54:35 +0200

    netstemplateengine1

--- lab4/netstemplateengine/SimpleTemplateEngine.h
+++ lab4/netstemplateengine/SimpleTemplateEngine.h
@@ -5,4 +5,21 @@
 #ifndef JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
 #define JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
 
+#include <iostream>
+#include <regex>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+namespace nets {
+    class View {
+    public:
+        View();
+        View(string nana_);
+        ~View();
+        string Render(const unordered_map <string, string> &model) const;
+    private:
+        string nana;
+    };
+}
 #endif //JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
