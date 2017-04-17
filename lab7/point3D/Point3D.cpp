549792be7171a5ff026762cdd9c0aa0d2805de45
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-04-18 00:21:46 +0200

    point3d1 (almost complete, 2.5/4)

--- lab7/point3D/Point3D.cpp
+++ lab7/point3D/Point3D.cpp
@@ -5,45 +5,81 @@
 #include "Point3D.h"
 
 #include <cmath>
-#include <iostream>
 
-using ::std::ostream;
-using ::std::endl;
-using ::std::pow;
-using ::std::sqrt;
-using ::std::cout;
-
-//Aby wskazać, ze definicja funkcji dotyczy metody danej klasy
-//stosujemy tzw. operator zasięgu - "::"
-
-//Specjalna inicjalizacja zmiennych. Zmienne są inicjowane
-//nim zostanie wywołane ciało konstruktora
+//2D
 Point::Point():x_(0),y_(0){
     cout << "Konstruktor bezparametrowy" << endl;
 }
 
 Point::Point(double x, double y){
-    cout << "Konstruktor parametrowy" << endl;
+    cout << "Konstruktor parametrowy 2d!" << endl;
     x_ = x;
     y_ = y;
 }
 
 Point::~Point(){
-    cout << "Destruktor! Nic nie robie, bo nie musze zwalniać pamięci!";
-    cout << endl;
-}
-
-double Point::GetX() const{
-    return x_;
-}
-double Point::GetY() const{
-    return y_;
+    cout << "Destruktor 2d!" << endl;
 }
 
 double Point::Distance(const Point &other) const{
     return sqrt(pow(GetX()-other.GetX(),2)+pow(GetY()-other.GetY(),2));
 }
 
-void Point::ToString(ostream *out) const{
-    (*out) << "(" << GetX() << ";" << GetY() << ")";
+double Point::GetX() const{
+    return x_;
+}
+
+double Point::GetY() const{
+    return y_;
+}
+
+void Point::SetX(double x_) {
+    Point::x_ = x_;
+}
+
+void Point::SetY(double y_) {
+    Point::y_ = y_;
+}
+
+void CheckNextChar(char c, istream* is) {
+    int next_char = is->peek();
+    if (next_char != c) {
+        throw runtime_error("invalid character");
+    }
+    is->ignore();
+}
+
+double ReadNumber(istream* is) {
+    double d;
+    (*is) >> d;
+    return d;
+}
+
+istream& operator>>(istream & input, Point& p){
+    CheckNextChar('(', &input);
+    p.SetX(ReadNumber(&input));
+    CheckNextChar(',', &input);
+    p.SetY(ReadNumber(&input));
+    CheckNextChar(')', &input);
+    return input;
+}
+
+//3D
+Point3D::Point3D(): Point() {}
+
+Point3D::Point3D(double x, double y, double z): Point(x, y){
+    cout << "Konstruktor parametrowy 3d!" << endl;
+    z_ = z;
+}
+
+Point3D::~Point3D(){
+    cout << "Destruktor 3d!" << endl;
+}
+
+double Point3D::GetZ() const{
+    return z_;
+}
+
+double Point3D::Distance(const Point3D &other) const{
+    return sqrt(pow(GetX()-other.GetX(),2)+pow(GetY()-other.GetY(),2)+pow(GetZ()-other.GetZ(),2));
 }
