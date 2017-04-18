63e3dda0e04b14bd7d907e4b50444fe473799f5d
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-04-18 23:15:37 +0200

    shape1

---
+++ lab7/shape/Shape.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Admin on 2017-04-18.
+//
+
+#include "Shape.h"
+
+void Circle::Draw() {
+    cout<<"( )"<<endl;
+}
+
+void Square::Draw(){
+    cout<<"[ ]"<<endl;
+}
+
+void BrokenHeart::Draw(){
+    cout<<"</3"<<endl;
+}
