f1cf4f1590d208f4789351d879bc2800603b6b87
Michał Wypych <mwypych@agh.edu.pl>
2017-05-11 18:52:15 +0200

    Adds schedule test cases

---
+++ tests/lab10test/ScheduleTest.cpp
@@ -0,0 +1,84 @@
+//
+// Created by mwypych on 11.05.17.
+//
+
+#include <gtest/gtest.h>
+#include <MemLeakTest.h>
+#include <Scheduler.h>
+
+using ::academia::Schedule;
+using ::academia::SchedulingItem;
+
+class ScheduleTest : public ::testing::Test, MemLeakTest {
+ public:
+};
+
+TEST_F(ScheduleTest, SchedulerInsertSchedulingItemReturnScheduleOfFirstTeacherWithSingleItem) {
+  Schedule schedule;
+  schedule.InsertScheduleItem(SchedulingItem {0, 1, 2, 3, 4});
+  schedule.InsertScheduleItem(SchedulingItem {5, 6, 7, 8, 9});
+
+  const Schedule &firstTeacherSchedule = schedule.OfTeacher(1);
+
+  EXPECT_EQ(1, firstTeacherSchedule.Size());
+  EXPECT_EQ(0, firstTeacherSchedule[0].CourseId());
+  EXPECT_EQ(1, firstTeacherSchedule[0].TeacherId());
+  EXPECT_EQ(2, firstTeacherSchedule[0].RoomId());
+  EXPECT_EQ(3, firstTeacherSchedule[0].TimeSlot());
+  EXPECT_EQ(4, firstTeacherSchedule[0].Year());
+}
+
+TEST_F(ScheduleTest, SchedulerInsertSchedulingItemReturnScheduleOfFirstTeacherWith3Items) {
+  Schedule schedule;
+  schedule.InsertScheduleItem(SchedulingItem {0, 1, 2, 3, 4});
+  schedule.InsertScheduleItem(SchedulingItem {5, 33, 7, 8, 9});
+  schedule.InsertScheduleItem(SchedulingItem {10, 1, 12, 13, 14});
+  schedule.InsertScheduleItem(SchedulingItem {15, 1, 17, 18, 19});
+  schedule.InsertScheduleItem(SchedulingItem {35, 35, 37, 38, 39});
+
+  const Schedule &firstTeacherSchedule = schedule.OfTeacher(1);
+
+  EXPECT_EQ(3, firstTeacherSchedule.Size());
+  EXPECT_EQ(0, firstTeacherSchedule[0].CourseId());
+  EXPECT_EQ(1, firstTeacherSchedule[0].TeacherId());
+  EXPECT_EQ(2, firstTeacherSchedule[0].RoomId());
+  EXPECT_EQ(3, firstTeacherSchedule[0].TimeSlot());
+  EXPECT_EQ(4, firstTeacherSchedule[0].Year());
+
+  EXPECT_EQ(10, firstTeacherSchedule[1].CourseId());
+  EXPECT_EQ(1, firstTeacherSchedule[1].TeacherId());
+  EXPECT_EQ(12, firstTeacherSchedule[1].RoomId());
+  EXPECT_EQ(13, firstTeacherSchedule[1].TimeSlot());
+  EXPECT_EQ(14, firstTeacherSchedule[1].Year());
+
+  EXPECT_EQ(15, firstTeacherSchedule[2].CourseId());
+  EXPECT_EQ(1, firstTeacherSchedule[2].TeacherId());
+  EXPECT_EQ(17, firstTeacherSchedule[2].RoomId());
+  EXPECT_EQ(18, firstTeacherSchedule[2].TimeSlot());
+  EXPECT_EQ(19, firstTeacherSchedule[2].Year());
+}
+
+TEST_F(ScheduleTest, SchedulerInsertSchedulingItemReturnScheduleOfSeventhRoomWith2Items) {
+  Schedule schedule;
+  schedule.InsertScheduleItem(SchedulingItem {0, 1, 2, 3, 4});
+  schedule.InsertScheduleItem(SchedulingItem {5, 33, 7, 8, 9});
+  schedule.InsertScheduleItem(SchedulingItem {10, 1, 12, 13, 14});
+  schedule.InsertScheduleItem(SchedulingItem {15, 4, 7, 18, 19});
+  schedule.InsertScheduleItem(SchedulingItem {35, 35, 37, 38, 39});
+
+  const Schedule &seventhRoomSchedule = schedule.OfRoom(7);
+
+  EXPECT_EQ(2, seventhRoomSchedule.Size());
+  EXPECT_EQ(5, seventhRoomSchedule[0].CourseId());
+  EXPECT_EQ(33, seventhRoomSchedule[0].TeacherId());
+  EXPECT_EQ(7, seventhRoomSchedule[0].RoomId());
+  EXPECT_EQ(8, seventhRoomSchedule[0].TimeSlot());
+  EXPECT_EQ(9, seventhRoomSchedule[0].Year());
+
+  EXPECT_EQ(15, seventhRoomSchedule[1].CourseId());
+  EXPECT_EQ(4, seventhRoomSchedule[1].TeacherId());
+  EXPECT_EQ(7, seventhRoomSchedule[1].RoomId());
+  EXPECT_EQ(18, seventhRoomSchedule[1].TimeSlot());
+  EXPECT_EQ(19, seventhRoomSchedule[1].Year());
+
+}
