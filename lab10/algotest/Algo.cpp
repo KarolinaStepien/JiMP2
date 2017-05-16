d38da23de9218974263d1bb836d1c7d913e83b83
stepkaro <kastepien@student.agh.edu.pl>
2017-05-16 11:01:22 +0200

    Merge branch 'master' of https://gitlab.com/agh-jimp2/exercises
    # Conflicts:
    #	CMakeLists.txt
    #	lab8/CMakeLists.txt

--- lab10/algotest/Algo.cpp
+++ lab10/algotest/Algo.cpp
@@ -4,6 +4,24 @@
 
 #include "Algo.h"
 
-void CopyInto(const std::vector<int> &v, int n_elements, std::vector<int> *out){
+using namespace algo;
 
+void Algo::SortInPlace(std::vector<int> *v){
+    std::sort(v->begin(), v->end());
+}
+
+std::vector<int> Algo::Sort(const std::vector<int> &v){
+    std::vector<int> l = v;
+    std::sort(l.begin(), l.end());
+    return l;
+}
+
+int Algo::Sum(const std::vector<int> &v){
+    int sum = std::accumulate(v.begin(), v.end(), 0);
+    return sum;
+}
+
+int Algo::Product(const std::vector<int> &v){
+    int product = std::accumulate(v.begin(), v.end(), 1, std::multiplies<int>());
+    return product;
 }
