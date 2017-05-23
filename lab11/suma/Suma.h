06e795ee05cbabca57e33784d84f2d631472fb56
stepkaro <kastepien@student.agh.edu.pl>
2017-05-23 11:06:16 +0200

    Merge branch 'master' of https://gitlab.com/agh-jimp2/exercises
    # Conflicts:
    #	CMakeLists.txt
    #	lab8/CMakeLists.txt

--- lab11/suma/Suma.h
+++ lab11/suma/Suma.h
@@ -5,10 +5,21 @@
 #ifndef JIMP_EXERCISES_SUMA_H
 #define JIMP_EXERCISES_SUMA_H
 
+#include <complex>
+#include <iostream>
 
-class Suma {
-
+template<class J>
+class Typ {
+public:
+    J x;
+    Typ operator + (Typ &cos){
+        return this->x + cos.x;
+    }
 };
 
+template<class T>
+T Suma( const T &left, const T &right){
+    return left + right;
+}
 
 #endif //JIMP_EXERCISES_SUMA_H
