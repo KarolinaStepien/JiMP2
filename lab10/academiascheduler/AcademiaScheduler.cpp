3090369dbe7d4613a35a485e84bc52b7c83e3b8d
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-06-10 13:52:15 +0200

    scheduler template

--- lab10/academiascheduler/AcademiaScheduler.cpp
+++ lab10/academiascheduler/AcademiaScheduler.cpp
@@ -3,3 +3,78 @@
 //
 
 #include "AcademiaScheduler.h"
+
+academia::SchedulingItem::SchedulingItem(int course_, int teacher_, int room_, int time_, int year_){
+    course_id = course_;
+    teacher_id = teacher_;
+    room_id = room_;
+    time_slot = time_;
+    year = year_;
+}
+
+/*int academia::SchedulingItem::CourseId() const {
+    return course_id;
+}
+
+int academia::SchedulingItem::TeacherId() const {
+    return teacher_id;
+}
+
+int academia::SchedulingItem::RoomId() const {
+    return room_id;
+}
+
+int academia::SchedulingItem::TimeSlot() const{
+    return time_slot;
+}
+
+int academia::SchedulingItem::Year() const {
+    return year;
+}*/
+
+academia::Schedule academia::Schedule::OfTeacher(int teacher_id) const {
+    Schedule st;
+
+    for(int i=0; i<items.size(), i++){
+        if(teacher_id == items[i].teacher_id){
+            st.items.emplace_back(items[i]);
+        }
+    }
+    return st;
+}
+
+academia::Schedule academia::Schedule::OfRoom(int room_id) const {
+    Schedule sr;
+
+    for(int i=0; i<items.size(), i++){
+        if(room_id == items[i].room_id){
+            sr.items.emplace_back(items[i]);
+        }
+    }
+    return sr;
+}
+
+academia::Schedule academia::Schedule::OfYear(int year) const {
+    Schedule sy;
+
+    for(int i=0; i<items.size(), i++){
+        if(year == items[i].year){
+            sy.items.emplace_back(items[i]);
+        }
+    }
+    return sy;
+}
+
+void academia::Schedule::InsertScheduleItem(const academia::SchedulingItem &item) {
+    SchedulingItem element = item;
+    taken_time_slots.emplace_back(element.time_slot);
+    items.emplace_back(element);
+}
+
+size_t academia::Schedule::Size() const {
+    return items.size();
+}
+
+/*academia::SchedulingItem academia::Schedule::operator[](int iter) const {
+    return items[iter];
+}*/
--- lab10/academiascheduler/AcademiaScheduler.h
+++ lab10/academiascheduler/AcademiaScheduler.h
@@ -5,4 +5,72 @@
 #ifndef JIMP_EXERCISES_ACADEMIASCHEDULER_H
 #define JIMP_EXERCISES_ACADEMIASCHEDULER_H
 
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+//#include <algorithm>
+//#include <string>
+
+namespace academia {
+
+    class SchedulingItem { //pojedynczy element harmonogramu, obiekt wartości
+    public:
+        SchedulingItem(int course_, int teacher_, int room_, int time_, int year_);
+        ~SchedulingItem(){};
+
+        int course_id; //identyfikator kursu
+        int teacher_id; //identyfikator nauczyciela
+        int room_id; //identyfikator pomieszczenia
+        int time_slot; //okno czasowe np 1 ⇒ pon 8:00-10:00
+        int year; //rok studiów na którym obowiązuje przedmiot
+
+        /*int CourseId() const;
+        int TeacherId() const;
+        int RoomId() const;
+        int TimeSlot() const;
+        int Year() const;*/
+    };
+
+    class Schedule { //cały harmonogram
+    public:
+        Schedule () {};
+        ~Schedule() {};
+        Schedule (std::vector <SchedulingItem> item_v);
+
+        Schedule OfTeacher(int teacher_id) const; //wylicza fragment harmonogramu związany z danym nauczycielem
+        Schedule OfRoom(int room_id) const; // -||- z danym pomieszczeniem
+        Schedule OfYear(int year) const; // -||- dla danego roku studiów
+        std::vector<int> AvailableTimeSlots(int n_time_slots) const;  //wylicza wektor jeszcze nie zajętych okien czasowych,
+        // gdzie n_time_slots jest maksymalną wartością okna czasowego
+        void InsertScheduleItem(const SchedulingItem &item); //wstawia nowy element planu
+        size_t Size() const; //zwraca rozmiar planu
+
+        std::vector<SchedulingItem> items;
+        std::vector<int> taken_time_slots;
+
+        //SchedulingItem operator[](int iter) const;
+    };
+
+    class Scheduler { //interfejs czysto abstrakcyjny stanowiący kontrakt między nami a zespołem
+    public:
+        virtual Schedule PrepareNewSchedule(const std::vector<int> &rooms, const std::map<int, std::vector<int>> &teacher_courses_assignment,
+                                    const std::map<int, std::set<int>> &courses_of_year, int n_time_slots) =0;
+        /*rooms - dostępne pomieszczenia
+        teacher_courses_assignment - rozpiska nauczycieli (klucz w mapie) i prowadząnych przez nich przedmiotów (wartosć w mapie)
+        courses_of_year - kursy (wartość w mapie) wymagane dla danego rocznika (klucz w mapie)
+        n_time_slots - ilość slotów czasowych*/
+    };
+
+    class NoViableSolutionFound { //wyjątek wyrzucany, gdy nie udaje się stworzyć planu
+
+    };
+
+    class GreedyScheduler { //pętla która próbuje przypisywać po kolei przedmioty do sal i nauczycieli
+    /*public:
+        Schedule PrepareNewSchedule(const std::vector<int> &rooms, const std::map<int, std::vector<int>> &teacher_courses_assignment,
+        const std::map<int, std::set<int>> &courses_of_year, int n_time_slots);*/
+    };
+
+}
 #endif //JIMP_EXERCISES_ACADEMIASCHEDULER_H
