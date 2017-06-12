3bcef4a7e5fce729bc1a3e99389c4c4b23399990
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-06-12 12:18:13 +0200

    scheduler main update

--- lab10/academiascheduler/main.cpp
+++ lab10/academiascheduler/main.cpp
@@ -11,6 +11,6 @@ int main(){
     schedule.InsertScheduleItem(SchedulingItem {1,2,3,4,5});
     schedule.InsertScheduleItem(SchedulingItem {1,1,3,4,5});
     schedule.InsertScheduleItem(SchedulingItem {1,2,3,4,6});
-    
+
     return 0;
 }
