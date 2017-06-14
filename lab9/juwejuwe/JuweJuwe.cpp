62c727f7271f4bba1dd423336765a788ed3fbcc5
KarolinaStepien <kastepien@student.agh.edu.pl>
2017-06-14 19:26:08 +0200

    Update JuweJuwe.cpp

--- lab9/juwejuwe/JuweJuwe.cpp
+++ lab9/juwejuwe/JuweJuwe.cpp
@@ -1,8 +1,8 @@
 //
-// Created by stepkaro on 09.05.17.
+// Created by zosiek on 23.05.17.
 //
 
-#include "JuweJuwe.h"
+#include "Juwe.h"
 
 Student::Student(){
     masa = 58;
@@ -10,45 +10,57 @@ Student::Student(){
     stala_woda = 0.58;
 }
 
+
+
 bool ChlEbac(Plecak cos, Student ktos) {
     double ebac;
-    ebac = ((9.672*cos.ilosc_napojow)/(ktos.stala_woda*ktos.masa))-0.17*ktos.czas_picia;
+    ebac = ((0.9672*cos.ilosc_napojow)/(ktos.stala_woda*ktos.masa))-0.17*ktos.czas_picia;
+
+   // std:: cout<<cos.ilosc_napojow<<"  "<<ebac<<std::endl;
     return ebac < 0.2;
 }
 
 void Zakupy(Plecak cos, Student Zosia) {
     srand(time(NULL));
 
-    while (cos.pojemnosc <= 20){
-        int wybor = rand()%4;
+    while (cos.pojemnosc < 20){
+        int wybor;
+        wybor = rand()%4;
+
         if (wybor == 1){
             SokPorzeczkowy tymbark;
-            cos.produkty.emplace_back(tymbark);
+            cos.produkty.push_back(tymbark);
             cos.pojemnosc += tymbark.obj;
+            //std::cout<<"1: " <<cos.ilosc_napojow<<std::endl;
         }
         if (wybor == 2){
             SunBites pomidorowe;
-            cos.produkty.emplace_back(pomidorowe);
+            cos.produkty.push_back(pomidorowe);
             cos.pojemnosc += pomidorowe.obj;
+            //std::cout<<"2: " <<cos.ilosc_napojow<<std::endl;
         }
         if (wybor == 3){
-            Wóda żubrówka;
-            cos.ilosc_napojow += (żubrówka.alko_gr/10);
+            Woda zubrowka;
+            cos.ilosc_napojow += (zubrowka.alko_gr/10);
             if(ChlEbac(cos, Zosia)){
-                cos.produkty.emplace_back(żubrówka);
-                cos.pojemnosc += żubrówka.obj;
+                cos.produkty.push_back(zubrowka);
+                cos.pojemnosc += zubrowka.obj;
             }
-            else cos.ilosc_napojow -= (żubrówka.alko_gr/10);
+            else cos.ilosc_napojow -= (zubrowka.alko_gr/10);
+          //  std::cout<<"3: " <<cos.ilosc_napojow<<std::endl;
         }
-        if (wybor == 4){
-            Piwerko żubr;
-            cos.ilosc_napojow += (żubr.alko_gr/10);
+        if (wybor == 0){
+            Piwerko zubr;
+            cos.ilosc_napojow += (zubr.alko_gr/10);
             if(ChlEbac(cos, Zosia)){
-                cos.produkty.emplace_back(żubr);
-                cos.pojemnosc += żubr.obj;
+                cos.produkty.push_back(zubr);
+                cos.pojemnosc += zubr.obj;
             }
-            else cos.ilosc_napojow -= (żubr.alko_gr/10);
+            else cos.ilosc_napojow -= (zubr.alko_gr/10);
+            //std::cout<<"0: " <<cos.ilosc_napojow<<std::endl;
         }
-    }
-}
 
+    }
+
+
+}
